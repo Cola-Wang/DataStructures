cca29a903defd40d1a1615dbf9f15c204b11f438
Cola-Wang <15881433445@163.com>
2020-06-13 17:47:59 +0800

    使用泛型

--- src/com/cola/Main.java
+++ src/com/cola/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Array arr = new Array(20);
+        Array<Integer> arr = new Array<>(20);
         for (int i = 0; i < 10; i++)
             arr.addLast(i);
         System.out.println(arr.toString());
 
-        arr.add(1,100);
+        arr.add(1, 100);
         System.out.println(arr.toString());
 
         arr.addFirst(-1);
--- src/com/cola/array/Array.java
+++ src/com/cola/array/Array.java
@@ -2,14 +2,14 @@ package com.cola.array;
 
 import java.util.Arrays;
 
-public class Array {
+public class Array<E> {
 
-    private int[] data;
+    private E[] data;
     private int size;
 
     // 构造函数，传入数组的容量capacity构造Array
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
@@ -34,17 +34,17 @@ public class Array {
     }
 
     // 在所有元素后添加一个新元素
-    public void addLast(int e) {
+    public void addLast(E e) {
         add(size, e);
     }
 
     // 在所有元素前添加一个新元素
-    public void addFirst(int e) {
+    public void addFirst(E e) {
         add(0, e);
     }
 
     // 在index位置插入一个新元素e
-    public void add(int index, int e) {
+    public void add(int index, E e) {
         if (size == data.length)
             throw new IllegalArgumentException("Add failed, Array is Full");
 
@@ -59,61 +59,62 @@ public class Array {
     }
 
     // 获取index索引位置的元素
-    public int get(int index) {
+    public E get(int index) {
         if (index < 0 || index >= size)
             throw new IllegalArgumentException("Get failed, Index is Illegal");
         return data[index];
     }
 
     // 修改index索引位置的元素为e
-    public void set(int index, int e) {
+    public void set(int index, E e) {
         if (index < 0 || index >= size)
             throw new IllegalArgumentException("Set failed, Index is Illegal");
         data[index] = e;
     }
 
     // 查找数组中是否有元素e
-    public boolean container(int e) {
+    public boolean container(E e) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == e)
+            if (data[i].equals(e))
                 return true;
         }
         return false;
     }
 
     // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
-    public int find(int e) {
+    public int find(E e) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == e)
+            if (data[i].equals(e))
                 return i;
         }
         return -1;
     }
 
     // 删除数组中索引为index的元素，返回删除的元素
-    public int remove(int index) {
+    public E remove(int index) {
         if (index < 0 || index >= size)
             throw new IllegalArgumentException("Add failed, Require index >= 0 and index < size");
 
-        int ret = data[index];
+        E ret = data[index];
         for (int i = index + 1; i < size; i++)
             data[i - 1] = data[i];
         size--;
+        data[size] = null; // loitering objects != memory leak
         return ret;
     }
 
     // 从数组中删除第一个元素，返回删除的元素
-    public int removeFirst() {
+    public E removeFirst() {
         return remove(0);
     }
 
     // 从数组中删除最后一个元素，返回删除的元素
-    public int removeLast() {
+    public E removeLast() {
         return remove(size - 1);
     }
 
     // 从数组中删除元素e
-    public void removeElement(int e) {
+    public void removeElement(E e) {
         int index = find(e);
         if (index != -1)
             remove(index);
