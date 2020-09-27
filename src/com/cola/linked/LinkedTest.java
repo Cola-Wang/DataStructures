1154518a804db497b07e4a80caa273dfa616a0c6
Cola-Wang <15881433445@163.com>
2020-09-27 17:26:17 +0800

    链表的遍历，查询和修改

---
+++ src/com/cola/linked/LinkedTest.java
@@ -0,0 +1,14 @@
+package com.cola.linked;
+
+public class LinkedTest {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2, 123);
+        System.out.println(linkedList);
+    }
+}
