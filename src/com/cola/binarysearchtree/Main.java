fc209117e23852c8a15be08e67da01e239df667c
wangzihao <wangzh0926@thundersoft.com>
2021-02-22 17:12:35 +0800

    二分搜索树的前序遍历

---
+++ src/com/cola/binarysearchtree/Main.java
@@ -0,0 +1,18 @@
+package com.cola.binarysearchtree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int i = 0; i < nums.length; i++) {
+            bst.add(nums[i]);
+        }
+
+        bst.preOrder();
+        System.out.println();
+
+        System.out.println(bst);
+    }
+}
