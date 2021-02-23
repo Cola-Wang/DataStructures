e8e344311da372415b8968b3944a90ba3f9e8106
wangzihao <wangzh0926@thundersoft.com>
2021-02-23 11:16:24 +0800

    删除二分搜索树中的最大元素和最小元素

--- src/com/cola/binarysearchtree/Main.java
+++ src/com/cola/binarysearchtree/Main.java
@@ -1,29 +1,50 @@
 package com.cola.binarysearchtree;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
 
-        int[] nums = {5, 3, 6, 8, 4, 2};
-        for (int i = 0; i < nums.length; i++) {
-            bst.add(nums[i]);
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++) {
+            bst.add(random.nextInt(10000));
         }
 
-        bst.preOrder();
-        System.out.println();
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            nums.add(bst.removeMin());
+        }
 
-        bst.preOrderNR();
-        System.out.println();
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) > nums.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMin test completed");
 
-        bst.inOrder();
-        System.out.println();
-
-        bst.postOrder();
-        System.out.println();
-
-        bst.levelOrder();
-        System.out.println();
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+//        for (int i = 0; i < nums.length; i++) {
+//            bst.add(nums[i]);
+//        }
+//
+//        bst.preOrder();
+//        System.out.println();
+//
+//        bst.preOrderNR();
+//        System.out.println();
+//
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
+//
+//        bst.levelOrder();
+//        System.out.println();
 //        System.out.println(bst);
     }
 }
