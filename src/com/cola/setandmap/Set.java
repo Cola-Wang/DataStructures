4a93f97d70af8268cf8047b43bff4987b022030d
wangzihao <wangzh0926@thundersoft.com>
2021-02-23 14:40:47 +0800

    基于二分搜索树的集合实现

---
+++ src/com/cola/setandmap/Set.java
@@ -0,0 +1,14 @@
+package com.cola.setandmap;
+
+public interface Set<E> {
+
+    void add(E e);
+
+    void remove(E e);
+
+    boolean contains(E e);
+
+    int getSize();
+
+    boolean isEmpty();
+}
